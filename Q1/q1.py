3fd962de4613656bd2b0e96289e401fb9d4bd0b5
arcondello <arcondello@gmail.com>
2015-01-28 15:12:43 -0500

    solutions to Q1

--- Q1/q1.py
+++ Q1/q1.py
@@ -5,3 +5,80 @@
 # Q2: How has the ratio of males/females changed over time? (optional--plot)
 # Q3: How popular was your name over time? (optional-- plot)
 # Q4: How has the number of letters in baby names changed over time? (optional-- plot)
+
+
+
+import pandas as pd
+import os
+import re
+import matplotlib.pyplot as plt
+
+
+
+years = [int(re.findall('\d{4}', file)[0]) for file in os.listdir('names') if re.search('\d{4}', file)]
+
+frames = []
+for yr in years:
+    df = pd.read_csv('names/yob%d.txt' % yr, names=['Name','Sex','Births'])
+    df['Year']=yr
+    frames.append(df)
+    
+names = pd.concat(frames, ignore_index=True)
+
+###################################################################################
+# Q1
+if False :
+    df = names[names['Year']==1914]
+    print 'Question 1:', df['Births'].sum(), 'Babies Born'
+
+###################################################################################
+# Q2
+if False:
+    year_list = []
+    ratio_list = []
+    for year, group in names.groupby('Year'):
+        year_list.append(year)
+        gen_dict = {}
+        for gender, gp in group.groupby('Sex'):
+            gen_dict[gender] = float(gp['Births'].sum())
+        ratio_list.append(gen_dict['M']/gen_dict['F'])
+    solution = zip(year_list, ratio_list)
+    plt.plot(solution)
+    plt.show()
+    
+###################################################################################
+# Q3
+if False:
+    my_name = 'Alexander'
+    year_list = []
+    count_list = []
+    for year, group in names[names['Name']==my_name].groupby('Year'):
+        year_list.append(year)
+        count_list.append(group['Births'].sum())
+    for yr in years :
+        if yr not in year_list :
+            year_list.append(yr)
+            count_list.append(0)
+            
+    plt.plot(year_list,count_list)
+    plt.show()
+    
+###################################################################################
+# Q3
+if True:
+    year_list = []
+    letter_list = []
+    for year, group in names.groupby('Year'):
+        year_list.append(year)
+        letter_list.append((group.apply(lambda row: len(row['Name']), axis=1)*group['Births']).sum()/float(group['Births'].sum()))
+    plt.plot(year_list,letter_list)
+    plt.show()
+        
+        
+
+
+
+
+
+
+
