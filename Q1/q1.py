94e09034611ae71f0741a7b9b0abf1feb0d076fa
arcondello <arcondello@gmail.com>
2015-01-28 13:54:57 -0500

    Initial commit for question 1

---
+++ Q1/q1.py
@@ -0,0 +1,7 @@
+# Day 1 is just taken from Python for Data Analysis chp 1. If you have trouble importing, you can follow the instructions there.
+# Download the US Census Data on baby names, 1880-present, from here:
+# http://www.ssa.gov/oact/babynames/limits.html
+# Q1: How many babies were born in 1914?
+# Q2: How has the ratio of males/females changed over time? (optional--plot)
+# Q3: How popular was your name over time? (optional-- plot)
+# Q4: How has the number of letters in baby names changed over time? (optional-- plot)
